fd1d000c81c4f00852a25de165116c3ba80f4284
smailikk <32063140+smailikk@users.noreply.github.com>
2017-11-29 13:47:55 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,377 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Moto.ru</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+  </head>
+  <body>
+
+    <!-- Start Top Bar -->
+    <div class="top-bar">
+      <div class="top-bar-left">
+        <ul class="menu">
+          <li class="menu-text"><a href="#">Moto.ru</a></li>
+        </ul>
+      </div>
+      <div class="top-bar-right">
+        <ul class="menu">
+          <input type="search" name="search" placeholder="Search.." class="animated-search-form">
+          <li><a href="file:///C:/Users/Acer/Downloads/smailik-master/smailik-master/log-n-load/index.html">Войти</a></li>
+          <li><a href="file:///C:/Users/Acer/Downloads/smailik-master/smailik-master/sign-up-login-form/index.html">Регистрация</a></li>
+        </ul>
+      </div>
+    </div>
+    <!-- End Top Bar -->
+
+<div class="md-tabs">
+  <div class="mobile-nav-bar title-bar">
+    <div class="title-bar-left">
+    </div>
+    <div class="title-bar-center">
+      <span class="title-bar-text">Автомобили</span>
+    </div>
+    <div class="title-bar-right">
+    </div>
+  </div>
+
+  <ul class="tabs" data-responsive-accordion-tabs="tabs small-accordion medium-tabs large-tabs" id="collapsing-tabs" data-allow-all-closed="true" data-multi-expand="true">
+    <li class="tabs-title is-active"><a href="#panel1c" aria-selected="true">Запчасти и аксессуары</a></li>
+    <li class="tabs-title"><a href="#panel2c">Грузовики и спецтехника </a></li>
+    <li class="tabs-title"><a href="#panel3c">Легковые автомобили</a></li>
+    <li class="tabs-title"><a href="#panel4c">Мотоциклы и мототехника</a></li>
+    <div class="slide"></div>
+  </ul>
+
+  <div class="tabs-content" data-tabs-content="collapsing-tabs">
+    <div class="tabs-panel is-active" id="panel1c">
+      <h4><a href="#">Запчасти</a></h4>
+      <h4><a href="#">Аксессуары</a></h4>
+      <h4><a href="#">Автокосметика и автохимия</a></h4>
+      <h4><a href="#">Аудио- и видеотехника</a></h4>
+    </div>
+    <div class="tabs-panel" id="panel2c">
+      <h4><a href="#">Автобусы</a></h4>
+      <h4><a href="#">Грузовики</a></h4>
+      <h4><a href="#">Прицепы</a></h4>
+      <h4><a href="#">Экскаваторы</a></h4>
+    </div>
+    <div class="tabs-panel" id="panel3c">
+      <h4><a href="#">Audi</a></h4>
+      <h4><a href="#">BMW</a></h4>
+      <h4><a href="#">Mercedes-Benz</a></h4>
+      <h4><a href="#">Volvo</a></h4>
+    </div>
+    <div class="tabs-panel" id="panel4c">
+      <h4><a href="#">Вездеходы</a></h4>
+      <h4><a href="#">Квадроциклы</a></h4>
+      <h4><a href="#">Снегоходы</a></h4>
+      <h4><a href="#">Мотоциклы</a></h4>
+    </div>
+  </div>
+</div>
+
+    <div class="grid-container">
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <center><h1>Продажа автомобилей в Ульяновске</h1></center>
+        </div>
+      </div>
+
+                <div class="card card-product-hover">
+  <hr />
+  <img src="https://www.auto-mgn.ru/resources/images/upload/000000192664bc1a1a35_990x550_C.jpg" alt="picture of admin dashboard" />
+  <div class="card-product-hover-icons">
+    <a href="#"><i class="fa fa-shopping-cart"></i></a>
+    <a href="#"><i class="fa fa-star-o"></i></a>
+    <a href="#"><i class="fa fa-share-alt"></i></a>
+  </div>
+  <div class="card-product-hover-details">
+    <h3 class="card-product-hover-title">Audi A7 Sportback</h3>
+    <span class="card-product-hover-price">2 000 000 млн рублей </span>
+  </div>
+</div>
+
+      <div class="grid-x grid-padding-x">
+        <div class="large-12 cell">
+          <div class="callout">
+            <h3>Свежие объявления авто</h3>
+            <div class="column small-12 large-6">
+          <h6 class="product-color-title">Сортировать</h6>
+            <form class="product-option-selection">
+                <select>
+                  <option value="apollo">По умолчанию</option>
+                  <option value="husker">По дате</option>
+                  <option value="starbuck">Дешевле</option>
+                  <option value="hotdog">Дороже</option>
+                </select>
+              </label>
+            </form>
+        </div>
+
+    <div class="ecommerce-product-slider orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+        <div class="row small-up-2 medium-up-4 large-up-5 align-center">
+            <div class="column">
+            <div class="product-card">
+        <div class="product-card-thumbnail">
+      <a href="#"><img src="https://images.toyota-europe.com/ru/vehicle/landcruiser100/02d3361a-1e4d-46b5-a053-8423f25d129f/width/555/height/277/scale-mode/1/padding/0/background-colour/fff/image-quality/70/day-exterior-27_070.jpg"/></a>
+    </div>
+            <p><a href="http://zurb.com/university/code-skills">Land Cruiser 200</a><br /><p>От 3 799 000 млн рублей</p></p>
+            <h4>Комментарий продавца</h4>
+            <p>Автосалон EVIAS - это новые автомобили от официальных дилеров 
+            Гарантия действует по всей территории РФ 
+            В наличие имеются другие цвета и комплектации </p>
+            <h4>Комплектация</h4>
+            <p>Комплектация: EXECUTIVE BLACK
+            Гидро-пневмоподвеска с регулировкой клиренса
+            Адаптивная система регулировки жесткости подвески
+            Затемненные светодиодные головные фары
+            Светодиодные противотуманные фары
+            Рейлинги с элементами матового хрома
+            Люк с электроприводом
+            Боковые подножки с подсветкой 
+            Шины 285/50 R20
+            Накладки на передний и задний бампер 
+            Эмблема специальной серии
+            Решетка радиатора частично окрашенная в цвет кузова 
+            Аудиосистема премиум класса JBL с 14 динамиками (CD/MP3/WMA/DVD)
+            USB/AUX
+            Bluetooth®
+            Адаптивный круиз-контроль
+            9” цветной дисплей с сенсорным управлением
+            Навигационная система на русском языке
+            Электрообогрев лобового стекла
+            Электрообогрев руля</p>
+
+                <div class="ecommerce-product-slider orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+    <div class="row small-up-2 medium-up-4 large-up-5 align-center">
+        <div class="column">
+            <div class="product-card">
+            <div class="product-card-thumbnail">
+      <a href="#"><img src="http://altera-auto.ru/upload/iblock/49e/49e07fd4b66c5d7c1ae5845dd33edefc.png"/></a>
+    </div>
+            <p><a href="http://zurb.com/university/code-skills">CHEVROLET ORLANDO</a><br /> <p> Механика от 642 000 рублей</p></p>
+            <h4>Комментарий продавца</h4>
+
+            <p>Спорт пакет AMG, панорамная крыша, спортивные сидения, разно широкие диски AMG, ЭКО режим. Не бит не крашен все в родной краске. В эксплуатации с 2015года.</p>
+            <h4>Комплектация</h4>
+             <h5>Безопасность</h5>
+           Антиблокировочная система (ABS)
+            Подушка безопасности водителя
+            Подушки безопасности боковые
+            Парктроник
+            <h5>Обзор</h5>
+            Электропривод зеркал
+            Датчик света
+            Обогрев зеркал
+            Противотуманные фары
+            Датчик дождя
+            <h5>Комфорт</h5>
+            Круиз-контроль
+                <div class="ecommerce-product-slider orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+      <div class="row small-up-2 medium-up-4 large-up-5 align-center">
+             <div class="column">
+             <div class="product-card">
+         <div class="product-card-thumbnail">
+       <a href="#"><img src="https://www.bmw.ru/content/dam/bmw/marketRU/bmw_ru/all-models/x-series/x1/2015/model-card/BMW-X1_ModelCard.png/_jcr_content/renditions/cq5dam.resized.img.585.low.time1456835645840.png"/></a>
+      </div>
+            <p><a href="https://www.bmw.ru/ru/ssl/configurator.html#/JG11/S02TF/F48X/esl/new/">BMW 1 СЕРИИ</a><br /> <p>1 500 000 млн рублей</p></p>
+            <h4>Комментарий продавца</h4>
+            <p>Автомобиль в хорошем состоянии, немецкая сборка. Комплект летних и зимних шин. Своевременное обслуживание. Непрокуренный салон.</p>
+            <h4>Комплектация</h4>
+
+            <h5>Безопасность</h5>
+            Антиблокировочная система (ABS)
+            Парктроник
+            Блокировка замков задних дверей
+            <h5>Обзор</h5>
+            Электропривод зеркал
+            Электрообогрев лобового стекла
+            <h5>Комфорт</h5>
+            Прикуриватель и пепельница
+            Бортовой компьютер
+            Кондиционер
+            Сиденье водителя: ручная регулировка
+            Регулировка сидений водителя по высоте
+            Парктроник задний
+            Сиденье пассажира: ручная регулировка
+            Регулировка сидений пассажира по высоте
+            Система «старт-стоп»
+            Датчик давления в шинах
+            Регулировка руля по высоте и вылету
+            Усилитель руля
+
+       </div>
+     </div>
+   </div>
+ </div>
+
+
+          <hr />
+
+          <h3>Подать объявление</h3>
+          <form>
+            <div class="grid-x grid-padding-x">
+              <div class="large-4 medium-4 cell">
+                <label>Марка</label>
+                <input type="text" placeholder="Ввести" />
+              </div>
+            </div>
+             <div class="grid-x grid-padding-x">
+              <div class="large-4 medium-4 cell">
+                <label>Модель</label>
+                <input type="text" placeholder="Ввести" />
+              </div>
+            </div>
+            <div class="grid-x grid-padding-x">
+              <div class="large-4 medium-4 cell">
+                <label>Год выпуска</label>
+                <input type="text" placeholder="Ввести" />
+              </div>
+              <div class="large-4 medium-4 cell">
+                <label>Пробег</label>
+                <input type="text" placeholder="Ввести" />
+              </div>
+             
+            <div class="grid-x grid-padding-x">
+              <div class="large-12 cell">
+                <label>Коробка передач</label>
+                <select>
+                  <option>Выбрать</option>
+                  <option value="husker">Механика</option>
+                  <option value="starbuck">Автомат</option>
+                  <option value="hotdog">Робот</option>
+                </select>
+              </div>
+            </div>
+             <div class="large-4 medium-4 cell">
+                <label>Телефон</label>
+                <input type="text" placeholder="Ввести" />
+              </div>
+         <div class="large-4 medium-4 cell">
+                <div class="grid-x">
+                  <label>Почта</label>
+                  <div class="input-group">
+                    <input type="text" placeholder="Ввести" class="input-group-field" />
+                    <span class="input-group-label">.com</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+        <div class="large-4 medium-4 cell">
+          <button type="button" class="primary button expanded search-button">
+                Подать
+              </button>
+              <hr />
+            <div class="translucent-form-overlay">
+            <form>
+              <h3>Поиск</h3>
+              <div class="row columns">
+                <label>Марка
+                  <select name="status" type="text" >
+                    <option>Выбрать</option>
+                    <option value="building">Иномарка</option>
+                    <option value="office">Отечественные</option>
+                    <option value="building">Audi</option>
+                    <option value="building">BMW</option>
+                    <option value="building">Lexus</option>
+                    <option value="building">Mercedes-Benz</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Тип автомобиля
+                  <select name="status" type="text">
+                    <option>Выбрать</option>
+                    <option value="building">С пробегом</option>
+                    <option value="rent">Новый</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Тип кузова
+                  <select name="status" type="text">
+                    <option>Выбрать</option>
+                    <option value="building">Седан</option>
+                    <option value="office">Хетчбэк</option>
+                    <option value="building">Универсал</option>
+                    <option value="building">Внедорожник</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row columns">
+                <label>Коробка передач
+                  <select name="status" type="text">
+                    <option>Выбрать</option>
+                    <option value="building">Механика</option>
+                    <option value="office">Автомат</option>
+                    <option value="building">Робот</option>
+                  </select>
+                </label>
+              </div>
+              <div class="row">
+                <label class="columns small-12">Сумма</label>
+                <div class="columns small-6">
+                  <input type="number" min="0" name="min" placeholder="Цена от">
+                </div>
+                   <div class="columns small-6">
+                  <input type="number" min="0" name="max" placeholder="Цена до">
+                </div>
+              </div>
+              <button type="button" class="primary button expanded search-button">
+                Найти
+              </button>
+           </form>
+           </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="three-column-footer-contact-form-container">
+  <footer class="three-column-footer-contact-form" data-equalizer data-equalize-by-row="true">
+    <div class="footer-left" data-equalizer-watch>
+      <div class="baseline">
+        <div class="contact-details">
+          <h6>© 2002—2017 Moto.ru  (Ульяновская область)</h6>
+          <p><i class="fa fa-phone fa-lg" aria-hidden="true"></i> Реклама на сайте. </p>
+          <p><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i> Техподдержка. </a></p>
+          <p><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> Авто Ульяновск. </p>
+        </div>
+        <div class="newsletter">
+          <div class="input-group">
+            <h5>Авто.мото.ру: всё об автомобилях</h5>
+          </div>
+          <p><i class="fa fa-map-marker fa-lg" aria-hidden="true"></i> Продажа автомобилей в Ульяновске – частные объявления, автосалоны Ульяновска, авторынки Ульяновска. </p>
+        </div>
+      </div>
+     </div>
+    <div class="footer-right" data-equalizer-watch>
+      <div class="baseline">
+        <img class="thumbnail" src="http://sferareklama.ru/uploads/smi/31feafa7c108370f2aa8i.jpg">
+        <h5>Контакты:</h5>
+        <h6>тел: +7902 213 05 54</h6>
+        <h6>тел: 8 842 555 55 55</h6>
+        <h5>Адрес:</h5>
+        <h6>Московское шоссе 129А</h6>
+        <div class="social">
+          <i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-google-plus-square fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-linkedin-square fa-2x" aria-hidden="true"></i>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
